37087fb18a9d096d3d3c4aa849e3343d00412544
Stas Lymar <stas.lymar@s-lymar.local>
2019-08-30 21:31:27 +0300

    add super simple cart functionality

--- src/card.js
+++ src/card.js
@@ -3,24 +3,63 @@ import React from 'react';
 export class Card extends React.Component {
     constructor(props) {
         super(props);
+        this.props = props;
         this.defaultTitle = 'No title';
         this.currencySign = '$';
         this.defaultPrice = '0';
+        this.price = this.getPrice();
+        this.onClickHandler = this.onClickHandler.bind(this)
+        this.addToCart = this.addToCart.bind(this)
+    }
+
+    getTitle() {
+        return this.value ? this.value.item.title : this.defaultTitle;
     }
 
     getPrice() {
         return this.currencySign + (this.props.item.price || this.defaultPrice);
     }
 
+    setPrice(price) {
+        this.price = this.currencySign + price;
+    }
+
+    getClassName () {
+        return this.state && this.state.isActive
+            ? 'card active'
+            : 'card';
+    }
+
+    onClickHandler(event) {
+        this.setState({
+            isActive: !this.state ? true : !this.state.isActive,
+        });
+        const price = Math.round((Math.random() * 100)).toFixed(2);
+        this.setPrice(price);
+        const item = this.props.fn(this.props.k);
+        console.log("item: ", item);
+    }
+
+    addToCart(event) {
+        event.stopPropagation();
+        // this.props.addToCart(this.props.item);
+        this.props.addToCart({
+            title: this.props.item.title,
+            price: this.price.slice(1)
+        });
+    }
+
     render() {
         return(
-            <div className='card'>
+            <div className={ this.getClassName() }
+                 onClick={this.onClickHandler}>
                 <h5>
-                    {this.props.item.title || this.defaultTitle}
+                    {this.getTitle()}
                 </h5>
                 <h6>
-                    {this.getPrice()}
+                    {this.price}
                 </h6>
+                <button onClick={this.addToCart}>buy</button>
             </div>
         );
     }
--- src/main-content.js
+++ src/main-content.js
@@ -25,29 +25,67 @@ export class MainContent extends React.Component {
                 price: '2348957'
             }
         ];
+        this.cart = [];
+        this.state = {
+            totalAmount: 0,
+            cart: []
+        };
+        const that = this;
+        const fnsToBind = ['addToCart', 'handleClick'];
+
+        fnsToBind.forEach((fn) =>
+            that[fn] = that[fn].bind(that));
     }
-    handleClick(index) {
-        console.log(index);
+
+    handleClick(key) {
+        return this.goods[key];
+    }
+
+    addToCart(item) {
+        this.cart.push(item);
+        const totalAmount = this.cart
+            .reduce((result, item) => {
+                result += +item.price || 0;
+                return result;
+        }, 0);
+        this.setState({
+            totalAmount,
+            cart: this.cart,
+        });
+    }
+
+    getTotalAmount() {
+        return this.state.totalAmount;
+    }
+
+    renderCart() {
+        const content = (
+            <div>
+                    <strong>Full price: {this.getTotalAmount()}</strong>
+                    <button type="submit"
+                        className="cart-btn">
+                        Go to cart({this.state.cart.length})
+                    </button>
+            </div>
+            );
+        return content;
     }
 
 
     render () {
         return(
             <div className='wrapper'>
+                {this.cart.length ? this.renderCart() : (<div>empty cart, add something</div>)}
                 <div className='content'>
-                    {this.goods.map(i => <Card item={i} onClick={() => this.handleClick(i)} />)}
+                    {this.goods.map((item, key) =>
+                    <Card
+                        item={item}
+                        k={key}
+                        key={key}
+                        addToCart={this.addToCart}
+                        fn={this.handleClick} />)}
                 </div>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                '
                 <img src={this.img} alt={this.props.alt} title={this.props.title} />
 
                 content
